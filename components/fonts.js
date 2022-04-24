eb0a2b1fe12458d7499d814e0d03997a26529e52
keremduran <keremduran@live.com>
2022-04-24 12:08:09 -0400

    M PLus font removed due to the huge css chunk

--- components/fonts.js
+++ components/fonts.js
@@ -1,6 +1,7 @@
 const Fonts = () => (
+  // Rounded with Japanese: https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@300;700&display=swap
   <style jsx global>{`
-    @import url('https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@300;700&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;700&display=swap');
   `}</style>
 )
 export default Fonts
